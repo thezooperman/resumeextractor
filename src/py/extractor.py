f68d6a8d74f6505fed874876a02d8016e248d415
Zooperman <joy.calcutta@gmail.com>
2018-12-10 08:56:35 +0530

    Changes: - Added read from pdf feature - Added pylint guidelines - Added basic exception handling

--- src/py/extractor.py
+++ src/py/extractor.py
@@ -3,6 +3,7 @@
 import errno
 import os
 import pathlib
+import PyPDF2
 
 from docx import Document
 
@@ -13,9 +14,10 @@ INPUT_PATH = BASE_DIR / 'input'
 class ReadFile:
     '''
     TODO: 
-        # Add code to read from excel
-        # Add code to read from csv 
+        # Unit Testing
+        # using spacy for NER
     '''
+
     def __init__(self, file_name=None):
         self.file_name = file_name
 
@@ -37,16 +39,49 @@ class ReadFile:
         return INPUT_PATH / self.file_name
 
     def read_docx(self):
+        ''' Read a Doc/Docx file and return contents as generator object'''
         construct_file_path = self.__construct_file_path()
-        dox = Document(construct_file_path)
-        for para in dox.paragraphs:
-            yield para.text
-        del dox
-        del construct_file_path
+        try:
+            dox = Document(construct_file_path)
+            if not dox:
+                raise ValueError(f'{self.file_name}',
+                                 errno.EBADFD, os.strerror(errno.EBADFD))
+            for para in dox.paragraphs:
+                yield para.text
+        finally:
+            del dox
+            del construct_file_path
+
+    def read_pdf(self):
+        ''' Read a PDF file and return contents as generator object'''
+        construct_file_path = self.__construct_file_path()
+        try:
+            fp = open(construct_file_path, 'rb')
+            pdf_reader = PyPDF2.PdfFileReader(fp)
+            if not pdf_reader:
+                raise ValueError(f'{self.file_name}',
+                                 errno.EBADFD, os.strerror(errno.EBADFD))
+            print(f'PDF Pages: {pdf_reader.numPages}')
+            page = pdf_reader.getPage(1)
+            yield page.extractText()
+        finally:
+            del pdf_reader
+            fp.close()
+            del fp
+            del construct_file_path
 
 
 if __name__ == '__main__':
+    print('Read DocX')
+    print('-' * 20)
     dummyFile = 'demo.docx'
     read_file = ReadFile(dummyFile)
     print(read_file.file_name)
     [print(p) for p in read_file.read_docx()]
+    del read_file
+    print('Read PDF')
+    print('-' * 20)
+    dummyFile = 'combinedminutes.pdf'
+    read_file = ReadFile(dummyFile)
+    print(read_file.file_name)
+    [print(p) for p in read_file.read_pdf()]
