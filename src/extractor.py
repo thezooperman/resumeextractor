e03acb494fcd2513287a878c979ff237e6ed94d1
Zooperman <joy.calcutta@gmail.com>
2018-12-10 12:00:50 +0530

    Changes: - Re-structured project - Added move to archive method - Added move to output method - Used Try-Finally in file handling codes

--- src/extractor.py
+++ src/extractor.py
@@ -9,10 +9,14 @@ from docx import Document
 
 BASE_DIR = pathlib.Path.cwd()
 INPUT_PATH = BASE_DIR / 'input'
+OUTPUT_PATH = BASE_DIR / 'output'
+ARCHIVE_PATH = BASE_DIR / 'archive'
 
 
-class ReadFile:
+class FileOperation:
     '''
+    Utility to read, write files.
+    Also has basic validations.
     TODO: 
         # Unit Testing
         # using spacy for NER
@@ -38,6 +42,26 @@ class ReadFile:
     def __construct_file_path(self):
         return INPUT_PATH / self.file_name
 
+    def __move_processed_to_archive(self):
+        archive_path = pathlib.Path(ARCHIVE_PATH)
+        try:
+            if not archive_path.exists():
+                archive_path.mkdir(parents=True, exist_ok=True)
+            os.rename(self.__construct_file_path(),
+                      archive_path / self.file_name)
+        finally:
+            del archive_path
+
+    def __write_to_output(self):
+        output_path = pathlib.Path(OUTPUT_PATH)
+        try:
+            if not output_path.exists():
+                output_path.mkdir(parents=True, exist_ok=True)
+            os.rename(self.__construct_file_path(),
+                      output_path / self.file_name)
+        finally:
+            del output_path
+
     def read_docx(self):
         ''' Read a Doc/Docx file and return contents as generator object'''
         construct_file_path = self.__construct_file_path()
@@ -47,7 +71,8 @@ class ReadFile:
                 raise ValueError(f'{self.file_name}',
                                  errno.EBADFD, os.strerror(errno.EBADFD))
             for para in dox.paragraphs:
-                yield para.text
+                yield para.text.strip()
+            self.__move_processed_to_archive()
         finally:
             del dox
             del construct_file_path
@@ -62,8 +87,10 @@ class ReadFile:
                 raise ValueError(f'{self.file_name}',
                                  errno.EBADFD, os.strerror(errno.EBADFD))
             print(f'PDF Pages: {pdf_reader.numPages}')
-            page = pdf_reader.getPage(1)
-            yield page.extractText()
+            for page in range(pdf_reader.numPages):
+                content = pdf_reader.getPage(page)
+                yield content.extractText().strip()
+            self.__move_processed_to_archive()
         finally:
             del pdf_reader
             fp.close()
@@ -75,13 +102,15 @@ if __name__ == '__main__':
     print('Read DocX')
     print('-' * 20)
     dummyFile = 'demo.docx'
-    read_file = ReadFile(dummyFile)
+    read_file = FileOperation(dummyFile)
     print(read_file.file_name)
     [print(p) for p in read_file.read_docx()]
     del read_file
     print('Read PDF')
     print('-' * 20)
     dummyFile = 'combinedminutes.pdf'
-    read_file = ReadFile(dummyFile)
+    read_file = FileOperation(dummyFile)
     print(read_file.file_name)
-    [print(p) for p in read_file.read_pdf()]
+    for idx, p in enumerate(read_file.read_pdf()):
+        if idx == 0:
+            print(p)
