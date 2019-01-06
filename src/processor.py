75a475458ff5918a7c8f99c5a6f4f1439464379c
Zooperman <joy.calcutta@gmail.com>
2019-01-06 22:12:38 +0530

    Changes: - Cleaned text from PDF - Read most docs successfully - Raghu Nanden.docx fails read --> TODO

--- src/processor.py
+++ src/processor.py
@@ -1,52 +1,67 @@
 # /usr/bin/env python
 
-import spacy
 from fileop import FileOperation, walk_dir
 from io import StringIO
 from bs4 import BeautifulSoup
+import spacy
+import string
 from spacy.matcher import Matcher
 
 nlp = spacy.load('en')
-
+printable = set(string.printable)
 
 def strip_html(content):
     soup = BeautifulSoup(content, features='lxml')
     return soup.get_text()
 
+def clean_text(text):
+    return ''.join([c for c in text if c in printable])
 
 def test_nlp():
-    file_name = 'Sakshi Seth.pdf'
+    try:
+        file_name = 'Sakshi Seth.pdf'
+        file_op = FileOperation(file_name)
+        sb = StringIO()
+        sb.write(''.join([clean_text(strip_html(line)).strip() for line\
+                                        in file_op.read_pdf()]))
+        matcher = Matcher(nlp.vocab)
+        pattern_phone = [{'SHAPE': 'ddddd'}, {'ORTH': '-', 'OP': '?'}, {'SHAPE': 'ddddd'}]
+        matcher.add('PHONE_NUMBER', None, pattern_phone)
+        cleaned_doc = nlp(sb.getvalue().strip())
+        #print(cleaned_doc)
+        matches = matcher(cleaned_doc)
+        #for t in cleaned_doc.ents:
+            #print(t, t.label_)
+        for match_id, start, end in matches:
+            print(doc[start:end].text)
+    finally:
+        if sb:
+            sb.close()
+    
+def trainData():
+    train_data = []
+    '''
+    for f in walk_dir():
+        fileOp = FileOperation(f)
+        sb = StringIO()
+        suffix = f.split('.')[1]
+        if suffix in ('docx', 'doc'):
+            sb.write(''.join([clean_text(line).strip() for line\
+                                        in fileOp.read_docx()]))
+        elif suffix == 'pdf':
+            hitFlag = True
+            for obj in fileOp.read_pdf():
+                pass
+        print(sb.getvalue().strip())
+        sb.close()
+    '''
+    file_name = 'Raghu Nanden.docx'
     file_op = FileOperation(file_name)
-    sb = StringIO()
-    sb.write(' '.join([line for line in file_op.read_pdf()]))
-    matcher = Matcher(nlp.vocab)
-    pattern_phone = [{'SHAPE': 'ddddd'}, {
-        'ORTH': '-', 'OP': '?'}, {'SHAPE': 'ddddd'}]
-    pattern_phone_with_country_code = [{'ORTH': '+', 'OP': '?'},
-                                       {'SHAPE': 'dd', 'OP': '?'},
-                                       {'ORTH': '-', 'OP': '?'},
-                                       {'SHAPE': 'ddddd'},
-                                       {'ORTH': '-', 'OP': '?'},
-                                       {'SHAPE': 'ddddd'}]
-    patterns = []
-    patterns.append(pattern_phone)
-    patterns.append(pattern_phone_with_country_code)
-    matcher.add('PHONE_NUMBER', None, pattern_phone_with_country_code)
-#     print(sb.getvalue())
-#     print(strip_html(sb.getvalue()))
-    # doc = nlp(strip_html(sb.getvalue()))
-    doc = nlp('Call me at +918884404883 or +91-8884404883 or 8884404883 or 88844-04883 or +91-88844-04883')
-    sb.close()
-#     print(doc.ents)
-    # for t in doc.ents:
-    #     print(t, t.label_)
-    print([t.text for t in doc])
-    matches = matcher(doc)
-
-    for match_id, start, end in matches:
-        span = doc[start:end]
-        print(span.text)
+    for line in file_op.read_docx():
+        print(line.strip())
 
 
 if __name__ == '__main__':
-    test_nlp()
+    trainData()
+    #test_nlp()
+
