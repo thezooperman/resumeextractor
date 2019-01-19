638999f8fe25a730e79f8f4c29a13dcbea882520
Zooperman <joy.calcutta@gmail.com>
2019-01-19 23:22:37 +0530

    Changes:- - Added method to read pdf

--- src/test.py
+++ src/test.py
@@ -2,9 +2,13 @@ try:
     from xml.etree.cElementTree import XML
 except ImportError:
     from xml.etree.ElementTree import XML
-import zipfile
 import string
+import zipfile
+from io import StringIO
 
+from bs4 import BeautifulSoup
+
+from fileop import FileOperation
 
 """
 Module that extract text from MS XML Word document (.docx).
@@ -20,6 +24,12 @@ blacklisted_punctuation = {"'", '\\', '"'}
 
 def clean_text(text):
     return ''.join([c for c in text if c in printable and c not in blacklisted_punctuation])
+
+
+def strip_html(content):
+    soup = BeautifulSoup(content, features='lxml')
+    return soup.get_text().strip()
+
 
 def get_docx_text(path):
     """
@@ -41,6 +51,20 @@ def get_docx_text(path):
     # return '\n'.join(paragraphs)
     return ' '.join(clean_text(_) for _ in ' '.join(paragraphs).split())
 
+
+def get_pdf_text(path):
+    fileOp = FileOperation(path)
+    paragraphs = []
+    for line in fileOp.read_pdf():
+        paragraphs.append(line)
+    string = StringIO()
+    s = ''.join(clean_text(strip_html(_))
+                for _ in fileOp.read_pdf())
+    string.write(' '.join(_ for _ in s.split()))
+    print(string.getvalue().strip())
+
+
 if __name__ == '__main__':
-    path = 'input/Soumik  Mazumder.docx'
+    path = 'input/Raghu Nanden.docx'
     print(get_docx_text(path))
+    # get_pdf_text(path)
